0abd24a3606eec59e52b41b819815e23dd9c0810
avillena <agustin.villena@gmail.com>
2025-04-22 20:10:55 -0400

    REFACTOR simpificando wx3.py

--- run_wx3.cmd
+++ run_wx3.cmd
@@ -1,5 +1,4 @@
-REM python wx3.py process   "c:\workspace\@recordings\20250417 Incubadora - Marcelo Diaz\20250417 Validacion Pitch con Marcelo Diaz.wav" --hf-token  %HF_TOKEN%
-REM  python wx3.py diarize   --hf-token  %HF_TOKEN% c:\workspace\@recordings\mini-obeya2.mp4
-REM  python wx3.py transcribe  c:\workspace\@recordings\mini-obeya2.mp4 
-REM  python wx3.py process   --hf-token  %HF_TOKEN% c:\workspace\@recordings\mini-obeya2.mp4 --speaker-names "Guy,Jim"
-python wx3.py process   --hf-token  %HF_TOKEN% "c:\workspace\@recordings\20250417 23people Fundamentos de Kanban\Ordenado\*.mp4"
+python wx3.py diarize   --hf-token  %HF_TOKEN% c:\workspace\@recordings\mini-obeya2.mp4
+python wx3.py transcribe  c:\workspace\@recordings\mini-obeya2.mp4 
+python wx3.py process   --hf-token  %HF_TOKEN% c:\workspace\@recordings\mini-obeya2.mp4 --speaker-names "Guy,Jim"
+REM python wx3.py process   --hf-token  %HF_TOKEN% "c:\workspace\@recordings\20250417 23people Fundamentos de Kanban\Ordenado\*.mp4"
